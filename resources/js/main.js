fbc6ae3a728afca8d322f9007510e8eb8e486691
unknown <you@example.com>
2022-02-02 23:19:22 +0600

    Land Page

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+
+
+/*DROP DOWN BUTTON TOGGLE*/
+var dropDown = document.querySelector(".dropdown")
+
+dropDown.addEventListener('click', function(){
+    dropDown.classList.toggle("active")
+});
+
+/*DROP DOWN BUTTON TOGGLE*/
